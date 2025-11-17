ed94f358f4ef63079c15e999531a97dc8ee81e94
iFralex <61825057+iFralex@users.noreply.github.com>
2025-11-17 13:24:48 +0100

    python log

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ import sys
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler(sys.stdout)],
     filename="./candidai.log",
 )
 
