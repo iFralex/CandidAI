e73779c4fa5b06fd75edf56c9550062c44441501
iFralex <61825057+iFralex@users.noreply.github.com>
2025-11-23 19:25:49 +0100

    CSP disabled

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Header globali
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'payment=(self "https://acs.revolut.com")'
+          }
+        ],
+      },
+    ];
+  },
+
+  // Config immagini remote
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.brandfetch.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.ctfassets.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pngdownload.io',
-      },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'cdn.brandfetch.io' },
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'images.ctfassets.net' },
+      { protocol: 'https', hostname: 'pngdownload.io' },
     ],
   },
 
+  // Riscritture URL
   async rewrites() {
     return [
       {
@@ -39,6 +38,7 @@ const nextConfig: NextConfig = {
     ];
   },
 
+  // ESLint e TypeScript
   eslint: {
     ignoreDuringBuilds: true,
   },
